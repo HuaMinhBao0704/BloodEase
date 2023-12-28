46d1f0e7ffd343a99949490eb6d4eb05c05654a3
ThVy02 <20522175@gm.uit.edu.vn>
2023-12-28 09:14:25 +0700

    backbtn báo

--- app/src/main/java/com/example/bloodeasebackup/NewsActivity.java
+++ app/src/main/java/com/example/bloodeasebackup/NewsActivity.java
@@ -1,33 +1,50 @@
 package com.example.bloodeasebackup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bloodeasebackup.fragments.NewsFragment;
 
 public class NewsActivity  extends AppCompatActivity {
+    ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bao1);
+
+        backBtn = findViewById(R.id.backBtn);
+
+//        backBtn.setOnClickListener(view -> {
+//            getOnBackPressedDispatcher().onBackPressed();
+//        });
+
+        backBtn.setOnClickListener(view -> {
+            // Tạo Intent để chuyển hướng đến trang chủ (BottomNavActivity)
+            Intent intent = new Intent(NewsActivity.this, BottomNavActivity.class);
+            startActivity(intent);
+            // Kết thúc CertificatesDetailActivity
+            finish();
+        });
     }
 
-    public void onBackPressed() {
-        // Lấy reference đến fragment bằng tag
-        NewsFragment newsFragment = (NewsFragment) getSupportFragmentManager().findFragmentByTag("NewsFragment");
-
-        if (newsFragment != null) {
-            // Thực hiện các thao tác cần thiết với fragment
-            // ...
-
-            // Đặc biệt hữu ích nếu bạn muốn popBackStack khi nhấn nút Back
-            getSupportFragmentManager().popBackStack();
-        } else {
-            // Gọi onBackPressed() mặc định nếu không có fragment
-            super.onBackPressed();
-        }
-    }
+//    public void onBackPressed() {
+//        // Lấy reference đến fragment bằng tag
+//        NewsFragment newsFragment = (NewsFragment) getSupportFragmentManager().findFragmentByTag("NewsFragment");
+//
+//        if (newsFragment != null) {
+//            // Thực hiện các thao tác cần thiết với fragment
+//            // ...
+//
+//            // Đặc biệt hữu ích nếu bạn muốn popBackStack khi nhấn nút Back
+//            getSupportFragmentManager().popBackStack();
+//        } else {
+//            // Gọi onBackPressed() mặc định nếu không có fragment
+//            super.onBackPressed();
+//        }
+//    }
 
 }
